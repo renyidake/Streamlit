02f8d5c6927b114363a25b5965e76d9bf53e12cd
大可 <11402440+renyidake@user.noreply.gitee.com>
2023-09-27 15:19:09 +0800

    Update Word文档格式调整.py

--- streamlit系列/Word文档格式调整.py
+++ streamlit系列/Word文档格式调整.py
@@ -28,7 +28,7 @@ temp_dir = tempfile.TemporaryDirectory()
 uploaded_files = st.file_uploader('上传需要更改格式的Word文件！', accept_multiple_files=True, type='docx')
 
 if not uploaded_files:
-    文件=Document('streamlit系列/练习原.docx')  # streamlit系列/2020-2022中之信.xlsx
+    wd=Document('streamlit系列/练习原.docx')  # streamlit系列/2020-2022中之信.xlsx
 else:
     for file in uploaded_files:
         # 将上传的文件保存到临时文件夹中
@@ -36,7 +36,7 @@ else:
             f.write(file.getbuffer())
         # 获取临时文件的完整路径
         excel_file_path = f"{temp_dir.name}/{file.name}"
-    文件 = Document(excel_file_path)
+    wd = Document(excel_file_path)
 
     st.write("请输入各部分的字体字号")
     ziti1=st.text_input("一级标题字体", value='黑体',key="ziti1")
@@ -66,58 +66,58 @@ else:
     st.write('一级标题:',ziti1,zihao1,'二级标题:',ziti2,zihao2,'三级标题:',ziti3,zihao3,'四级标题:',ziti4,zihao4,'正文:',ziti5,zihao5,'题注:',ziti6,zihao6)
 
     print('读取一级标题：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'Heading 1':
             print(段落.text)
     print('读取二级标题：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'Heading 2':
             print(段落.text)
     print('读取所有标题：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if re.match("^Heading \d+$",段落.style.name):
             print(段落.text)
 
     print('读取标题名称：')
-    标题 = 文件.styles
+    标题 = wd.styles
     for i in 标题:
         if i.type==WD_STYLE_TYPE.PARAGRAPH:
             print(i.name)
     print('Caption：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'Caption':
             print(段落.text)
     print('annotation text：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'annotation text':
             print(段落.text)
     print('toc 3：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'toc 3':
             print(段落.text)
     print('Footer：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'Footer':
             print(段落.text)
     print('Header：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'Header':
             print(段落.text)
 
     print('toc 2：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'toc 2':
             print(段落.text)
     print('Normal (Web)：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'Normal (Web)':
             print(段落.text)
     print('TOC Heading：')
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name == 'TOC Heading':
             print(段落.text)
 
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name =='Heading 1':
             for 块 in 段落.runs:
                 块.font.bold = True  # 加粗
@@ -132,7 +132,7 @@ else:
                 # 英文字体设置
                 块._element.rPr.rFonts.set(qn('w:eastAsia'),ziti1)
                 # 设置中文字体
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name =='Heading 2':
             for 块 in 段落.runs:
                 块.font.bold = True  # 加粗
@@ -147,7 +147,7 @@ else:
                 # 英文字体设置
                 块._element.rPr.rFonts.set(qn('w:eastAsia'),ziti2)
                 # 设置中文字体
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name =='Heading 3':
             for 块 in 段落.runs:
                 块.font.bold = True  # 加粗
@@ -162,7 +162,7 @@ else:
                 # 英文字体设置
                 块._element.rPr.rFonts.set(qn('w:eastAsia'),ziti3)
                 # 设置中文字体
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name =='Heading 4':
             for 块 in 段落.runs:
                 块.font.bold = True  # 加粗
@@ -177,7 +177,7 @@ else:
                 # 英文字体设置
                 块._element.rPr.rFonts.set(qn('w:eastAsia'),ziti4)
                 # 设置中文字体
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name =='Normal':
             for 块 in 段落.runs:
                 块.font.bold = False  # 加粗
@@ -192,7 +192,7 @@ else:
                 # 英文字体设置
                 块._element.rPr.rFonts.set(qn('w:eastAsia'),ziti5)
                 # 设置中文字体
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name =='TOC Heading':
             for 块 in 段落.runs:
                 块.font.bold = True  # 加粗
@@ -207,7 +207,7 @@ else:
                 # 英文字体设置
                 块._element.rPr.rFonts.set(qn('w:eastAsia'),'黑体')
                 # 设置中文字体
-    for 段落 in 文件.paragraphs:
+    for 段落 in wd.paragraphs:
         if 段落.style.name =='Caption':
             for 块 in 段落.runs:
                 块.font.bold = True  # 加粗
@@ -237,8 +237,8 @@ else:
     # Normal (Web)
     # TOC Heading  标题 自定义？
 
-    文件.save(r'D:\工具自动化产出\word文档格式调整.docx')
+    wd.save(r'D:\工具自动化产出\word文档格式调整.docx')
     # st.write('Word文档已存储于：D:\工具自动化绘图\word文档格式调整.docx')
     st.markdown("<span style='color:red'>Word文档已存储于：D:\工具自动化产出\word文档格式调整.docx</span>", unsafe_allow_html=True)
-
+temp_dir.cleanup()
 
